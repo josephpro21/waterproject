5799384f9b1c0ec6353c6d7a7a11a55a9cf6f75b
Josephpro21 <elasujoseph46@gmail.com>
2021-05-04 15:49:52 +0300

    code

--- group2BSEexe3.py
+++ group2BSEexe3.py
@@ -1,4 +1,4 @@
-'''#This is the part for the residential
+#This is the part for the residential
 customer_code = input("Enter customer code: ")
 beginning_meter_reading = int(input("Enter beginning meter reading: "))
 ending_meter_reading = int(input("Enter ending meter reading:    "))
@@ -17,32 +17,11 @@ if customer_code == 'r':
   #  first_amount = 1000.00
    # second_amount = 0.00025*Gallons_of_water_used
    # amount = first_amount+second_amount
-# This is the part for the commercial part c
 print("Amount billed:", round(amount,2))
 print("")
-customer_code1 = input("Enter customer code: ")
-beginning_meter_reading1 = int(input("Enter beginning meter reading: "))
-ending_meter_reading1 = int(input("Enter ending meter reading:    "))
-print("")
-print("Customer code: ", customer_code1)
-print("Beginning meter reading: ", beginning_meter_reading1)
-print("Ending meter reading:    ", ending_meter_reading1)
-beginning_meter_reading1 = 1000000000-beginning_meter_reading1
-ending_meter_reading1 = ending_meter_reading1
-Gallons_of_water_used = (ending_meter_reading1+beginning_meter_reading1)*0.1
-if Gallons_of_water_used <=4000000:
-    print("Gallons of water used: ", round(Gallons_of_water_used,1))
-else: print('error')
 
-if customer_code1 == 'c':
-    first_amount1 = 1000.00
-    second_amount1 = 0.00025*Gallons_of_water_used
-    amount1 = first_amount1+second_amount1
-    print("Amount billed:", round(amount1,2))
-print("\n")
+
 # This is the section for the industrial part i
-print("Amount billed:", round(amount,2))
-print("")
 customer_code1 = input("Enter customer code: ")
 beginning_meter_reading1 = int(input("Enter beginning meter reading: "))
 ending_meter_reading1 = int(input("Enter ending meter reading:    "))
@@ -75,40 +54,44 @@ ending_meter_reading1 = ending_meter_reading1
 Gallons_of_water_used1 = (ending_meter_reading1+beginning_meter_reading1)*0.1
 if Gallons_of_water_used1 <=4000000:
     print("Gallons of water used: ", round(Gallons_of_water_used1,1))
-else: print('error')
-
+#else: print('error')
 if customer_code1 == 'c':
     first_amount1 = 1000.00
     second_amount1 = 0.00025*Gallons_of_water_used1
     amount1 = first_amount1+second_amount1
-    print("Amount billed:", round(amount1,2))
-print("\n") '''
+print("Amount billed:", round(amount1,2))
+print("\n")
+
+
 # This is the section for the industrial part i
-print("")
-customer_code2 = input("Enter customer code: ")
+
+customer_code2 = input("Enter Customer Code: ")
 beginning_meter_reading2 = int(input("Enter beginning meter reading: "))
 ending_meter_reading2 = int(input("Enter ending meter reading:    "))
 print("")
 print("Customer code: ", customer_code2)
-print("Beginning meter reading: ", beginning_meter_reading2)
-print("Ending meter reading:    ", ending_meter_reading2)
-Gallons_of_water_used2 = (ending_meter_reading2 - beginning_meter_reading2)*0.1
 if customer_code2 == 'i':
- if Gallons_of_water_used2 <= 4000000:
-    print("Gallons of water used: ", round(Gallons_of_water_used2,1))
+ print("Beginning meter reading: ", beginning_meter_reading2)
+ print("Ending meter reading:    ", ending_meter_reading2)
+ Gallons_of_water_used2 = (ending_meter_reading2 - beginning_meter_reading2)*0.1
+if Gallons_of_water_used2 <= 4000000:
+    print("Gallons of water used: ", round(Gallons_of_water_used2,2))
     first_amount2 = 1000.00
-    second_amount2 = 0.00025*Gallons_of_water_used2
-    amount2 = first_amount2+second_amount2
-    print("Amount billed:", round(amount2,2))
-elif Gallons_of_water_used2 > 4000000 & Gallons_of_water_used2 <= 10000000 :
+    #second_amount2 = 0.00025*Gallons_of_water_used2
+    amount2 = first_amount2#+second_amount2
+    print("Amount billed: $", round(first_amount2,2))
+elif (Gallons_of_water_used2 > 4000000) and (Gallons_of_water_used2 <= 10000000) :
+    print("Gallons_of_water_used: ", Gallons_of_water_used2)
     amount2 = 2000.00
-    print("Amount billed: ",amount2)
-else:
+    print("Amount billed: $",amount2)
+elif Gallons_of_water_used2 > 10000000:
+    print("Gallons_of_water_used: ", Gallons_of_water_used2)
     first_amount2 = 2000.00
-    second_amount2 = 0.00025*Gallons_of_water_used2
+    second_amount2 = (0.00025)*Gallons_of_water_used2
     amount2 = first_amount2+second_amount2
-print("Amount billed", amount2)
+print("Amount billed: ", round(amount2,2))
 
+print("=========================================Thanks for using the program=========================================")
 
 
 
